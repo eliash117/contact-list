7e576272a6de2906fa4e62bb6136b947ff340cd8
eliash117 <139942614+eliash117@users.noreply.github.com>
2023-11-25 00:10:12 -0600

    Add files via upload

---
+++ pryecto integrado I/lista_de_contactos.js
@@ -0,0 +1,24 @@
+const contactos = ["Juan Pérez", "María García", "Pedro Sánchez"];
+function añadirContacto(lista, contacto) {
+    lista.push(contacto);
+  }
+  function borrarContacto(lista, contacto) {
+    const index = lista.indexOf(contacto);
+  
+    if (index !== -1) {
+      lista.splice(index, 1);
+    }
+  }
+  function imprimirContactos(lista) {
+    
+    for (const contacto of lista) {
+      console.log(contacto);
+    }
+  }
+
+  añadirContacto(contactos, "Ana López");
+  
+  borrarContacto(contactos, "María García");
+  
+  imprimirContactos(contactos);
+        
